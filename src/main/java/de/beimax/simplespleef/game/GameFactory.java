ee8ae8e78269006aa011098d0006c2a909113207
Max Kalus <max@beimax.de>
2011-12-31 13:21:50 +0100

    Implement game factory creation of classes other than "standard".

--- src/main/java/de/beimax/simplespleef/game/GameFactory.java
+++ src/main/java/de/beimax/simplespleef/game/GameFactory.java
@@ -3,6 +3,12 @@
  */
 package de.beimax.simplespleef.game;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+
+import de.beimax.simplespleef.SimpleSpleef;
+
 /**
  * @author mkalus
  * Factory class to create games
@@ -14,8 +20,26 @@ public abstract class GameFactory {
 	 * @param name
 	 * @return game or null, if game type does not exist
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Game createGame(String type, String name) {
-		// TODO: inject other game types
-		return new GameImpl(name);
+		// standard games
+		if (type == null || type.equalsIgnoreCase("standard"))
+			return new GameImpl(name);
+		// inject other game types
+		try {
+			// define constructor parameters
+			Class[] argsClass = new Class[] { String.class };
+			// define arguments for this constructor
+			Object[] args = new Object[] { new String(name) };
+			// use reflection to create new instance
+			Class gameClass = Class.forName(name); // get class
+			Constructor constructor = gameClass.getConstructor(argsClass); // get constructor
+			// call constructor and return game instance
+			return (Game) constructor.newInstance(args);
+		} catch (Exception e) {
+			SimpleSpleef.log.severe("[SimpleSpleef] Could not load class " + name);
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
